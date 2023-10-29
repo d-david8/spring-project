3b904d8b4ec9d9a429f197a61e60d2697ed429c8
Dan David <d_david8@yahoo.com>
2023-10-29 19:58:58 +0200

    homework unit tests

--- src/test/java/ro/ddavid/springproject/unit_tests/CalculatorTest.java
+++ src/test/java/ro/ddavid/springproject/unit_tests/CalculatorTest.java
@@ -4,8 +4,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import ro.ddavid.springproject.utils.Calculator;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
     private static Calculator calculator;
@@ -42,4 +41,96 @@ class CalculatorTest {
         //then
         assertNotEquals(expectedResult, actualResult);
     }
+
+    @Test
+    void testSubtract_WithValidInput_ShouldPass() {
+        //given
+        int a = 1;
+        int b = 1;
+        int expectedResult = a + b;
+
+        //when
+        int actualResult = calculator.add(a, b);
+
+        //then
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    void testSubtract_WithInvalidInput_ShouldFail() {
+        //given
+        int a = -100;
+        int b = Integer.MAX_VALUE;
+        long expectedResult = (long) a - b;
+
+        //when
+        int actualResult = calculator.subtract(a, b);
+
+        //then
+        assertNotEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    void testMultiply_WithValidInput_ShouldPass() {
+        //given
+        int a = 2;
+        int b = 3;
+        int expectedResult = a * b;
+
+        //when
+        int actualResult = calculator.multiply(a, b);
+
+        //then
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    void testMultiply_WithValidInput_ShouldFail() {
+        //given
+        int a = 3;
+        int b = 2;
+        int expectedResult = a * b * 2;
+
+        //when
+        int actualResult = calculator.multiply(a, b);
+
+        //then
+        assertNotEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    void testDivide_WithValidInput_ShouldPass() {
+        //given
+        int a = 33;
+        int b = 3;
+        int expectedResult = a / b;
+
+        //when
+        int actualResult = calculator.divide(a, b);
+
+        //then
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    void testDivide_WithValidInput_ShouldFail() {
+        //given
+        int a = 33;
+        int b = 10;
+        float expectedResult = (float) a / b;
+
+        //when
+        int actualResult = calculator.divide(a, b);
+
+        //then
+        assertNotEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    void testDivide_DivideByZero_ShouldThrowException() {
+        int a = 3;
+        int b = 0;
+        assertThrows(ArithmeticException.class, () -> calculator.divide(a, b));
+    }
 }
+
